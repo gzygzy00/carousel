c13dcd0e37c3fb87027a9ec461bd791d33809a86
gzy <283211498@qq.com>
2019-02-18 19:46:21 +0800

    init js

---
+++ js/index.js
@@ -0,0 +1,23 @@
+function Carousel($ct) {
+  this.init($ct)
+  this.bind()
+}
+
+Carousel.prototype = {
+  init: function () {
+
+  },
+
+  bind: function () {
+
+  },
+
+  render: function () {
+
+  }
+}
+
+new Carousel($(".carousel").eq(0))
+new Carousel($(".carousel").eq(1))
+
+console.log("ok");
