b5df8e731f97636dfe2cafeeacf866b9add8099d
gzy <283211498@qq.com>
2019-02-19 11:05:53 +0800

    完善js的初始化

--- js/index.js
+++ js/index.js
@@ -4,8 +4,18 @@ function Carousel($ct) {
 }
 
 Carousel.prototype = {
-  init: function () {
+  init: function ($ct) {
+    this.$ct = $ct
+    this.$imgCt = this.$ct.find('.img-ct')
+    this.$imgs = this.$ct.find('.img-ct>li')
+    this.$leftBtn = this.$ct.find('.left')
+    this.$rightBtn = this.$ct.find('.right')
+    this.$bullets = this.$ct.find('.bullet li')
 
+    this.$imgCt.append(this.$imgs.first().clone())
+    this.$imgCt.prepend(this.$imgs.last().clone())
+
+    //this.$imgCt.width((this.$imgs.length() + 2) * this.$imgs.width())
   },
 
   bind: function () {
@@ -17,7 +27,7 @@ Carousel.prototype = {
   }
 }
 
-new Carousel($(".carousel").eq(0))
-new Carousel($(".carousel").eq(1))
+new Carousel($('.carousel').eq(0));
+// new Carousel($('.carousel').eq(1));
 
 console.log("ok");
