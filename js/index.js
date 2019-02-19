293a704137af3380bf3a050ce07eaa7b67a9d925
gzy <283211498@qq.com>
2019-02-19 16:34:19 +0800

    add playRight & playLeft

--- js/index.js
+++ js/index.js
@@ -14,10 +14,15 @@ Carousel.prototype = {
     this.imgCount = this.$imgs.length
     this.imgWidth = this.$imgs.width()
 
+    this.index = 0
+    this.isPlay = 0
+
     this.$imgCt.append(this.$imgs.first().clone())
     this.$imgCt.prepend(this.$imgs.last().clone())
 
-    //this.$imgCt.width((this.imgCount + 2) * this.imgWidth)
+    this.$imgCt.width((this.imgCount + 2) * this.imgWidth)
+    this.$imgCt.css('left', -this.imgWidth)
+    //console.log("hh");
   },
 
   bind: function () {
@@ -36,15 +41,45 @@ Carousel.prototype = {
   },
 
   playLeft: function () {
+    console.log("playleft");
+    var _this = this
+    if (!this.isPlay) {
+      this.isPlay = 1
+      console.log("playright");
+      this.$imgCt.animate({
+        left: "-=" + this.imgWidth
+      }, function () {
+        _this.index++
+        if (_this.index === _this.imgCount) {
+          _this.index = 0
+          _this.$imgCt.css('left', -_this.imgWidth)
+        }
+        _this.isPlay = 0
+      })
+    }
 
   },
 
-  playRight: function(){
-
+  playRight: function () {
+    var _this = this
+    if (!this.isPlay) {
+      this.isPlay = 1
+      console.log("playright");
+      this.$imgCt.animate({
+        left: "+=" + this.imgWidth
+      }, function () {
+        _this.index--
+        if (_this.index < 0) {
+          _this.index = _this.imgCount - 1
+          _this.$imgCt.css('left', -_this.imgWidth * _this.imgCount)
+        }
+        _this.isPlay = 0
+      })
+    }
   }
 }
 
-new Carousel($('.carousel').eq(0));
+var a = new Carousel($('.carousel').eq(0));
 // new Carousel($('.carousel').eq(1));
 
 console.log("ok");
