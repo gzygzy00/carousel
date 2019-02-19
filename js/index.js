58f5597d2aa7cbf863eba32b8e3b247e91f06e14
gzy <283211498@qq.com>
2019-02-19 11:33:29 +0800

    修改，添加 bind 内容

--- js/index.js
+++ js/index.js
@@ -11,18 +11,35 @@ Carousel.prototype = {
     this.$leftBtn = this.$ct.find('.left')
     this.$rightBtn = this.$ct.find('.right')
     this.$bullets = this.$ct.find('.bullet li')
+    this.imgCount = this.$imgs.length
+    this.imgWidth = this.$imgs.width()
 
     this.$imgCt.append(this.$imgs.first().clone())
     this.$imgCt.prepend(this.$imgs.last().clone())
 
-    //this.$imgCt.width((this.$imgs.length() + 2) * this.$imgs.width())
+    //this.$imgCt.width((this.imgCount + 2) * this.imgWidth)
   },
 
   bind: function () {
+    var _this = this
+    this.$leftBtn.on('click', function () {
+      console.log("left");
+      _this.playLeft()
+    })
+    this.$rightBtn.on('click', function () {
+      console.log("right");
+      _this.playRight()
+    })
+    this.$bullets.on('click', function () {
+      console.log($(this).index());
+    })
+  },
+
+  playLeft: function () {
 
   },
 
-  render: function () {
+  playRight: function(){
 
   }
 }
